cd762532e451875bcfaf0eba22aef83c5d3fd1d1
hsemple <hsemple@emich.edu>
2024-09-06 18:21:13 -0400

    New  commit

--- docs/conf.py
+++ docs/conf.py
@@ -4,18 +4,14 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 project = 'Python GIS Tutorials'
 copyright = '2024, Hugh Semple'
 author = 'Hugh Semple'
 release = '0.0.0.1'
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
 extensions = [
-    'sphinx.ext.autodoc',  # Automatically document code
+    'sphinx.ext.autodoc',   # Automatically document code
     'sphinx.ext.napoleon',  # Support for Google style and NumPy style docstrings
     # Add other extensions as needed
 ]
@@ -24,9 +20,7 @@ templates_path = ['_templates']
 exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'alabaster'
+html_theme = 'bizstyle'
 
 # Add custom static files
 html_static_path = ['_static']
@@ -39,8 +33,8 @@ html_css_files = [
 # Optional: Customize theme options
 html_theme_options = {
     'description': 'Introductory tutorials to get started with Python for GIS',
-    'fixed_sidebar': True,
-    'sidebar_collapse': True,
-    'show_powered_by': False,
-    # Add other options as needed
+    'rightsidebar': False,
+    'stickysidebar': True,
+    'sidebarwidth': '250px',
+    'relbarbgcolor': 'black',
 }
